fdf5d4626e395c01ed1c12e7d5d60959b930c279
roilabs <w@pyszko.net>
2016-10-18 16:01:05 +0100

    gr

---
+++ index.js
@@ -0,0 +1,17 @@
+﻿var express = require('express');
+
+var app = express();
+
+
+app.get('/locations', function (req, res) {
+    console.log('reviews was requested...');
+    $http.get('data/locations.txt')
+    //res.json('data/locations.txt');
+});
+
+
+
+var port = process.env.PORT || 8000;
+app.listen(port, function() {
+    console.log('Server started on port ' + port);
+});
